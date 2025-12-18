f49f70d09707574e4b728efa8d5da2b8f60391ad
Sohal Sinha <94367698+sohalsinha@users.noreply.github.com>
2025-12-18 21:21:42 +0530

    Max Memory Changes setup

--- src/main/java/com/learning/openai/config/ChatMemoryClientConfig.java
+++ src/main/java/com/learning/openai/config/ChatMemoryClientConfig.java
@@ -6,6 +6,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,13 @@ import java.util.List;
 
 @Configuration
 public class ChatMemoryClientConfig {
+
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
+        //Using MAX Message to Limit Chat History for the user.
+        return  MessageWindowChatMemory.builder().maxMessages(10)
+                .chatMemoryRepository(repository).build();
+    }
 
     @Bean("chatMemoryChatClient")
     public ChatClient ChatMemoryClientConfig(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
